2411126ff54de205f4fad53d90fbb0cdf4c0e54c
Nathan764 <nathanoli330@gmail.com>
2022-03-16 17:31:57 -0300

    imersao-01

---
+++ funcoes.js
@@ -0,0 +1,11 @@
+var nome = "Nathan"
+var notaDoPrimeiroBimestre = 9
+var notaFinal = notaDoPrimeiroBimestre = 7
+var notaDoTerceiroBimestre = 4
+var notaDoQuartoBimestre = 2
+
+var notFinal = (notaDoPrimeiroBimestre + notaDoSegundoBimenstre + notaDoTerceiroBimestre + notaDoQuartoBimestre) / 4
+
+var notaFixada = notaFinal.toFixed(2)
+console.log("Bem vindo" + nome)
+console.log(notaFinal)
